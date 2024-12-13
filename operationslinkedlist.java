2b6df7eeb12cac5561b6ded069e4848c1defa309
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-13 15:20:16 +0530

    Add files via upload

---
+++ operationslinkedlist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class operationslinkedlist {
+    public static void main(String args[])
+    {
+        LinkedList<String> a=new LinkedList<>();
+        a.add("Hello");
+        a.add("I am");
+        a.add("Student");
+        System.out.println(a);
+        a.remove(0);
+        System.out.println(a);
+        a.clear();
+        System.out.println(a);
+    }
+    
+}
