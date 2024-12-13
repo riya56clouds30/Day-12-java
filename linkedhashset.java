c75578a13d27ca00f23d81ddfb46d11eff7ece3c
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-13 15:19:39 +0530

    Add files via upload

---
+++ linkedhashset.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class linkedhashset {
+    public static void main(String args[])
+    {
+      LinkedHashSet<String> a=new LinkedHashSet<>();
+      a.add("Hello");
+      a.add("Hello");
+      a.add("Students");
+      a.add("Rajesh");
+      a.add("Students");
+      System.out.println(a);
+      a.remove("Rajesh");
+      System.out.println(a);
+      a.clear();
+      System.out.println(a);
+    }
+    
+}
