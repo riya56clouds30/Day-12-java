616a63e84652db56ae3251c4339918e2774b9830
PRIYA <priyapriya07264@gmail.com>
2024-12-15 13:59:14 +0530

    Add files via upload

---
+++ treemapwithdata.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class treemapwithdata{
+    public static void main(String args[]){
+        TreeMap<String,String> a=new TreeMap<>();
+        a.put("SNO","01");
+        a.put("SName","priya");
+        System.out.println(a);
+    }
+}        
